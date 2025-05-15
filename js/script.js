00f8ab7707271987bf664ff36641f8ddb4e37208
PintilieGeani <pintilie.geany@yahoo.it>
2025-05-15 12:57:53 +0200

    feat: photo-js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 const getApi = "https://lanciweb.github.io/demo/api/pictures/"
+let card = document.getElementById("card")
+console.log(card)
+axios.get(getApi).then((resp) =>{
+    console.log(resp)
+    const photoArr = resp.data
+    console.log(photoArr)  
+    photoArr.forEach(curElem => {
+        console.log(curElem)
+       createCard(curElem)
+    });
+})
+
+
+function createCard(elemento){
+     const {title, date, url} = elemento
+    card.innerHTML += `<div class="col">
+                    <div class="card">
+                        <div class="pin">
+                            <img src="./img/pin.svg" alt="pin">
+                        </div>
+                        <div class="card-image">
+                            <img src="${url}" alt="${title}">
+                        </div>
+                        <div class="card-text">
+                            <p>${title}</p>
+                        </div>
+                    </div>
+                </div>`
+}
